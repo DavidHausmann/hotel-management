a15ebc9cc816977af5f07e43f15ad48ca96a109b
david.hausmann <davidwilliamhausmann@gmail.com>
2025-10-28 23:26:54 -0300

    filter guests

--- frontend/src/shared/components/filter-component/filter-component.component.ts
+++ frontend/src/shared/components/filter-component/filter-component.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { ThemeService } from '../../../core/services/theme/theme.service';
 
 @Component({
   selector: 'app-filter-component',
-  imports: [],
   templateUrl: './filter-component.component.html',
-  styleUrl: './filter-component.component.scss'
+  styleUrls: ['./filter-component.component.scss'],
+  standalone: true,
+  imports: [CommonModule, MatButtonModule],
 })
-export class FilterComponentComponent {
+export class FilterComponent {
+  @Output() applyFiltersEvent = new EventEmitter<void>();
+  @Output() clearFiltersEvent = new EventEmitter<void>();
+  @Output() closeEvent = new EventEmitter<void>();
 
+  constructor(public themeService: ThemeService) {}
+
+  isDarkMode(): boolean {
+    return this.themeService.isDarkMode();
+  }
+
+  onClick(e: Event) {
+    // stop clicks from bubbling to outer UI if used inside overlays
+    e.stopPropagation();
+  }
+
+  applyFilters() {
+    this.applyFiltersEvent.emit();
+  }
+
+  clearFilters() {
+    this.clearFiltersEvent.emit();
+  }
+
+  close() {
+    this.closeEvent.emit();
+  }
 }
+
